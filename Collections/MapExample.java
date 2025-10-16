26cc6324c74d60cf1fa97e85c5a79028880ad378
prasanth <prasanthb313@gmail.com>
2025-10-16 16:29:01 +0530

    16th october - collections

--- Collections/MapExample.java
+++ Collections/MapExample.java
@@ -10,6 +10,14 @@ public class MapExample {
         /*
            Map - > it is an interface
                stores data in the form of key value pair
+
+
+               Map
+                 | Hash Map
+                 | LinkedHashMap
+                 | Tree Map
+                 | Hash Table (not allows null value)
+
          */
 
         HashMap<String,Integer> hashMap =  new HashMap<String,Integer>();
@@ -18,7 +26,7 @@ public class MapExample {
 
         hashMap.put("Prasanth",123);
         hashMap.put("Nellore",989);
-        hashMap.put("India",91);
+        hashMap.put("India",null);
 
         //try to add more countries and codes into the map
         System.out.println(hashMap);
@@ -32,6 +40,20 @@ public class MapExample {
 
         //if country code is more than 50 then print all the countries along with codes
 
+        // occurrence of each character in given string
+
+        // Sravanthi
+
+        /*
+            S 1
+            r 1
+            a 2
+            v 1
+            t 1
+            n 1
+            h 1
+            i 1
+         */
 
     }
 }
